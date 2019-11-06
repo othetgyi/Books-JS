2a24e207a2c52647eceb32faf3a372f84f4cbf25
Olivia Thetgyi <50234696+othetgyi@users.noreply.github.com>
2019-11-06 22:01:21 +0000

    Added function to search for books given parameters

---
+++ index_withkey.js
@@ -0,0 +1,70 @@
+//App functions:
+//1.) Ask what book user wants to look up
+//2.) Call the Google Books API with the query
+//3.) Prints out 5 choices of books with author, title and publishing company
+//4.) Program asks user which one to put on the bookshelf
+//5.) Program stores those books by pushing the new book onto the end of the array
+
+const request = require('request');
+const fs = require('fs');
+
+//Requires the readline module for taking user input
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+//Asks what book user wants to look up
+let book;
+
+initialPrompt() => {
+    readline.question(`What book are you looking for? `, (book) => {
+    //calls API with the search string
+    console.log(book);
+    callAPI();
+    readline.close()
+    })
+}
+
+//Calls Google books API with the search string and API key
+callAPI()=> {
+    let key = //API key here
+
+    request('https://www.googleapis.com/books/v1//volumes?q=' + book + '&fields=title, author, publisher&maxResults=5' + '&key=' + key, { json: true }, (err, res, data) => {
+  if (err) { return console.log(err); }
+  console.log(data);
+ // saveQuestion();
+});
+}
+/*
+//Asks user what book to save to a "Reading List"
+saveQuestion() => {
+    readline.question(`Which book do you want to save in your reading list? `, (book) => {
+        //calls function to save book to reading list
+        saveReadingList();
+        readline.close()
+    })
+}
+
+//Takes that response and pushes the book onto an array
+saveReadingList() => {
+    let readingList = [];
+    readingList.push(1);
+    viewReadingList();
+    
+}
+
+//Gives user option to view "Reading List"
+viewReadingList() => {
+    readline.question(`Do you want to view your reading list? Enter Y or N `, (book) => {
+        if ('y') {
+            console.log(readingList);
+        }
+       else {
+           initialPrompt();
+       }
+        readline.close()
+        initialPrompt();
+    })
+}
+
