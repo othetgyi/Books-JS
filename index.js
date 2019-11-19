8b6fe21f5aff27f7f94236688b056b0fe7e25309
Olivia Thetgyi <50234696+othetgyi@users.noreply.github.com>
2019-11-19 22:09:51 +0000

    Added back two parameters for getSearchTerm

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ require('dotenv').config();
     //viewReadingList();
 
 function main(){
-    getSearchTerm(function(){
-        validateSearchTerm(function(){
+    getSearchTerm(function(searchTerm){
+        validateSearchTerm(function(searchTerm){
             printIt(searchTerm);
         });
     });
@@ -32,11 +32,12 @@ var rl = readline.createInterface({
 
 //Asks what kind of book the user wants to look up
 var getSearchTerm = function(searchTerm, callback) {
-    rl.question('What books are you looking for? ', function(searchTerm, callback){
+    rl.question('What books are you looking for? ', function(searchTerm) {
         console.log(`You're looking for ${searchTerm} books.`);
         console.log('Function getSearchTerm is finished.');
-    }); 
-    callback(searchTerm);
+        });
+        callback(searchTerm);    
+    
 }
 
 //Validates user input 
